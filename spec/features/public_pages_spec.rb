fc45a9354e912a512629dd957c0832515217d19e
Derrick Lannaman <derricklannaman@gmail.com>
2015-08-17 18:03:11 -0500

    remove duplicate path calls

--- spec/features/public_pages_spec.rb
+++ spec/features/public_pages_spec.rb
@@ -1,24 +1,25 @@
+require 'pry'
+
 describe 'Public-facing pages' do
 
   context 'when visiting the home page' do
+    before do
+      visit root_path
+    end
+
     it 'shows the home page' do
-      visit(root_path)
       expect(page).to have_css('body.data-controller-home')
     end
 
     it 'has a navigation bar with links' do
-      visit(root_path)
       expect(page).to have_selector('nav')
     end
 
     it 'has navigation links' do
-      visit(root_path)
       expect(page).to have_link('about')
       expect(page).to have_link('menu')
       expect(page).to have_link('news')
     end
-
-
   end
 
 end
