8177c321d3954b67d5e174c2fba73c78ab65e743
Derrick Lannaman <derricklannaman@gmail.com>
2015-08-16 19:45:59 -0500

    More spec setup in config file..first spec

---
+++ spec/features/public_pages_spec.rb
@@ -0,0 +1,14 @@
+describe 'home page' do
+
+  context 'Visiting the home page' do
+
+    it 'shows the home page' do
+      visit "http://www.example.com/"
+      expect(page).to have_content "Kula"
+    end
+
+
+  end
+
+
+end
