ab0b87835e5fd795d0da50abe8fc522cbbd60c5f
Derrick Lannaman <derricklannaman@gmail.com>
2015-08-17 17:53:38 -0500

    add basic nav links

--- spec/features/public_pages_spec.rb
+++ spec/features/public_pages_spec.rb
@@ -2,9 +2,23 @@ describe 'Public-facing pages' do
 
   context 'when visiting the home page' do
     it 'shows the home page' do
-      visit "http://www.example.com/"
+      visit(root_path)
       expect(page).to have_css('body.data-controller-home')
     end
+
+    it 'has a navigation bar with links' do
+      visit(root_path)
+      expect(page).to have_selector('nav')
+    end
+
+    it 'has navigation links' do
+      visit(root_path)
+      expect(page).to have_link('about')
+      expect(page).to have_link('menu')
+      expect(page).to have_link('news')
+    end
+
+
   end
 
 end
