b2b919e7858d699af90e953f57ca2e5bf1fcdb65
Derrick Lannaman <derricklannaman@gmail.com>
2015-08-25 13:36:56 -0500

    fix registration page for using devise, add registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   root 'home#index'
+
   get 'about', to: 'home#about'
   get 'news', to: 'home#news'
   get 'destinations', to: 'home#destinations'
