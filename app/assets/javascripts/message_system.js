0bea3f06ea7f09a407ea27b8be916955d591685f
Derrick Lannaman <derricklannaman@gmail.com>
2015-09-23 08:32:05 -0500

    add multiple carousels

--- app/assets/javascripts/message_system.js
+++ app/assets/javascripts/message_system.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
 
   // messageBarStatus();
+$('[id^="myCarousel"]').carousel({
+  pause: true,
+  interval: false
+});
+// $('.myCarousel').carousel()
+
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+
+
+
 
 });
 
@@ -16,3 +39,4 @@ function messageBarStatus() {
   }
 
 }
+
